baef301cd6a51e336258e29ed220a7dc3b8ae907
Martin Yrjölä <martin.yrjola@gmail.com>
2022-04-16 12:16:41 +0300

    Connect with database

--- backend/create_bingo/main.py
+++ backend/create_bingo/main.py
@@ -1,3 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 def create_bingo(request):
     """
     Args:
@@ -6,4 +10,22 @@ def create_bingo(request):
         The response text, or any set of values that can be turned into a
         Response object using `make_response`
     """
-    return "Hello World!"
+    print(request)
+
+    cred = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(cred, {
+        'projectId': 'djurbingo',
+    })
+
+    db = firestore.client()
+
+    doc_ref = db.collection(u'users').document(u'alovelace')
+    doc_ref.set({
+        u'first': u'Ada',
+        u'last': u'Lovelace',
+        u'born': 1815
+    })
+
+    alovelace = db.collection(u'users').document(u'alovelace')
+
+    return alovelace.get().to_dict()
